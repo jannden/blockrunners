3b6718990a8ff9e4f10f748ded66a808de74291b
IhorMuliar <goga440@gmail.com>
2025-03-20 19:40:50 +0200

    2: Add player managment logic

--- programs/blockrunners/src/lib.rs
+++ programs/blockrunners/src/lib.rs
@@ -2,10 +2,11 @@
 
 use anchor_lang::prelude::*;
 
-declare_id!("D3Z8EKaXQssyvFBcVLuSPnj5xVywsxCujqd9wxgbuEkU");
+declare_id!("2gt4rq13cxxjWBhfrdA8yXbhTwaqYAettE5rEuquqjgp");
 
 const INITIAL_PRIZE_POOL: u64 = 0;
 const INITIAL_PATH_LENGTH: u8 = 20;
+const INITIAL_PLAYER_CARDS_AMOUNT: u64 = 1;
 
 #[program]
 pub mod blockrunners {
@@ -18,6 +19,17 @@ pub mod blockrunners {
         game_state.path_length = INITIAL_PATH_LENGTH;
 
         msg!("Game initialized.");
+        Ok(())
+    }
+
+    pub fn join_game(ctx: Context<JoinGame>) -> Result<()> {
+        let player_state = &mut ctx.accounts.player_state;
+
+        player_state.ciphers = 0;
+        player_state.cards = INITIAL_PLAYER_CARDS_AMOUNT; // Start with 1 card
+        player_state.position = 0;
+
+        msg!("Player joined the game.");
         Ok(())
     }
 }
@@ -39,6 +51,23 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct JoinGame<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + 8 + 8 + 1, // discriminator + ciphers + cards + position
+        seeds = [b"player_state", authority.key().as_ref()],
+        bump
+    )]
+    pub player_state: Account<'info, PlayerState>,
+
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 pub struct GameState {
     /// The current prize pool amount in lamports
@@ -46,4 +75,16 @@ pub struct GameState {
 
     /// The length of the path players need to navigate (fixed at 20 for alpha)
     pub path_length: u8,
+}
+
+#[account]
+pub struct PlayerState {
+    /// Number of ciphers owned
+    pub ciphers: u64,
+
+    /// Number of cards 
+    pub cards: u64,
+
+    /// Current block number (fixed at 20 for alpha)
+    pub position: u8,
 }
