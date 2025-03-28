de5cee28dbe3887a8823e3e913d15f843707eead
Ihor Muliar <31934552+IhorMuliar@users.noreply.github.com>
2025-03-28 22:47:45 +0200

    feat: Remove redundant instructions (#34)

--- programs/blockrunners/src/instructions/mod.rs
+++ programs/blockrunners/src/instructions/mod.rs
@@ -1,15 +1,11 @@
 pub mod save_and_emit_event;
 pub mod generate_player_path;
-pub mod get_game_state;
-pub mod get_player_state;
 pub mod initialize_game;
 pub mod initialize_player;
 pub mod purchase_ciphers;
 
 pub use save_and_emit_event::*;
 pub use generate_player_path::*;
-pub use get_game_state::*;
-pub use get_player_state::*;
 pub use initialize_game::*;
 pub use initialize_player::*;
 pub use purchase_ciphers::*;
