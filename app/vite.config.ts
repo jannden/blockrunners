726d92816c9e15a1ebf3e8d6db0b66bab4b85690
Jan Cibulka <jc@pntr.eu>
2026-01-05 09:58:25 +0200

    fix(app): Add base path for GH pages

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/blockrunners/",
   plugins: [react(), nodePolyfills()],
   resolve: {
     alias: {
